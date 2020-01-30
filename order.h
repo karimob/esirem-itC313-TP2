593b31ac1d72445d337a725981e55fbb3fa0a304
karimob <56952989+karimob@users.noreply.github.com>
2020-01-30 01:30:40 +0100

    classe commande inachevé

---
+++ order.h
@@ -0,0 +1,30 @@
+#ifndef _order_h
+#define _order_h
+#include "client2.h"
+#include "produit2.h"
+//client prodiuts  statut
+
+class Order{
+
+
+
+public:
+
+
+
+
+
+
+private:
+
+std::string client;
+std::vector<Produit> m_prodcom;
+std::string m_statut;
+
+
+
+};
+
+
+
+#endif
